27ccf39fb881cc5aed7ff3f8a153c47b81bce4a6
potala tarun <potala.tarun.19cse@bmu.edu.in>
2023-06-03 08:40:32 +0530

    2nd version of the solution which take the order of n time complexity

--- Merge_two_sortedLIsts.cpp
+++ Merge_two_sortedLIsts.cpp
@@ -37,3 +37,53 @@ public:
         return head=head->next;
     }
 };
+
+
+
+
+// 2nd version of solution 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head=new ListNode(0);
+        head->next=NULL;
+        ListNode *temp=head;
+        // temp=temp->next;
+        while(list1!=NULL or list2!=NULL){
+            if(list1==NULL){
+                temp->next=list2;
+                return head->next;
+            }
+            if(list2==NULL){
+                temp->next=list1;
+                return head->next;
+            }
+            if(list1->val<=list2->val){
+                ListNode *newnode = new ListNode(list1->val);
+                newnode->next=NULL;
+                temp->next=newnode;
+                temp=temp->next;
+                list1=list1->next;
+            }
+            else {
+                ListNode *newnode = new ListNode(list2->val);
+                newnode->next=NULL;
+                temp->next=newnode;
+                temp=temp->next;
+                list2=list2->next;
+            }
+        }
+        return head->next;
+    }
+};
